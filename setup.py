5e0da1bcc58e2d26c0771c4e5d6eb1cf173965f3
Tapas <Tapas@AAIZELTECH.COM>
2026-02-18 12:22:23 +0530

    folder struct'

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.1.0",
+    author="Tapas Barman",
+    author_email="tapasb.dev@gmail.com",
+    packages=find_packages()
+)
